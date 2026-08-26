3a00d81573fb4d8cb3f7286b2b04bb63176762b7
Erickalvarado56 <erickalvaradorod09@gmail.com>
2026-08-25 21:21:38 -0600

    operadores basicos

--- Operadores/src/operadoresaritmeticos.java
+++ Operadores/src/operadoresaritmeticos.java
@@ -1,5 +1,21 @@
 public class operadoresaritmeticos {
     public static void main(String[] args) {
-
+        System.out.println("Operadores aritmeticos");
+        double a = 5, b = 3, resultado = 0;
+        // Suma +
+        resultado = a+ b;
+        System.out.println(" resultado de suma = " + resultado);
+        // Resta -
+        resultado = a - b;
+        System.out.println("resultado de resta = " + resultado);
+        // multiplicacion *
+        resultado = a * b;
+        System.out.println("resultado de multiplicacion = " + resultado);
+        // Division /
+        resultado = a / b;
+        System.out.println("resultado de la division = " + resultado);
+        // Modulo (residuo division %)
+        resultado = a % b;
+        System.out.println("resultado del modulo = "+ resultado );
     }
 }
