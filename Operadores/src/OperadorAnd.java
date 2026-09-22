9f3cb39cda8a194643cd4bab0b7adcfcb85dc162
Erickalvarado56 <erickalvaradorod09@gmail.com>
2026-09-21 19:57:39 -0600

    OperadoresAR

---
+++ Operadores/src/OperadorAnd.java
@@ -0,0 +1,10 @@
+public class OperadorAnd {
+    public static void main(String[] args) {
+        System.out.println("Operador And");
+        boolean a = true, b = true;
+        // and (regresa true si ambos valores dan true
+        var resultado = a && b;
+        System.out.println("Resultado a&&b: " + resultado);
+
+    }
+}
---
+++ Operadores/src/OperadorOrlogico.java
@@ -0,0 +1,9 @@
+public class OperadorOrlogico {
+    public static void main(String[] args) {
+        System.out.println("Operador Or");
+        boolean a = false;
+        //Operador not ! invierte el valor logico
+        var resultado = !a;
+        System.out.println("Resultado !a: "+resultado);
+    }
+}
