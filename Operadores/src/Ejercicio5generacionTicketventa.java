63c5c84236cf68cd90ae9784a873a8377ffd22f6
Erickalvarado56 <erickalvaradorod09@gmail.com>
2026-09-24 15:59:41 -0600

    ejercicios

---
+++ Operadores/src/Ejercicio5generacionTicketventa.java
@@ -0,0 +1,11 @@
+public class Ejercicio5generacionTicketventa {
+    public static void main(String[] args) {
+        //Ejercicio 5: Supongamos que compramos varios articulos en el supermercado
+        //y queremos obtener el ticket de venta total incluyendo impuestos.
+        //El sistema solicitará el precio de cada producto a comprar y el usuario
+        //deberá indicar su precio (valor de tipo con punto decimal)
+        //El sistema debe realizar la suma de cada producto, imprimir el total
+        //de la compra 
+
+    }
+}
